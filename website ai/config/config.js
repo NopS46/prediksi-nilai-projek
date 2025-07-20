f64f1a5e2be520c37a5970a5abc7157c5724882b
NopS46 <cumanthaka4@gmail.com>
2025-07-21 02:28:54 +0700

    Add files via upload

---
+++ website ai/config/config.js
@@ -0,0 +1,6 @@
+const config = {
+    apiUrl: "http://localhost:8000/api/predict",
+    defaultClass: "X TKJ 1",
+    defaultProject: "Pembuatan Kabel Straight dan Cross",
+    googleFormUrl: ""
+};
