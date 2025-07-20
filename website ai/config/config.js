a2aecf8bc6cec7e3ac04dda7651480ca87ab5fdb
NopS46 <cumanthaka4@gmail.com>
2025-07-21 02:36:31 +0700

    Delete website ai directory

--- website ai/config/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-    apiUrl: "http://localhost:8000/api/predict",
-    defaultClass: "X TKJ 1",
-    defaultProject: "Pembuatan Kabel Straight dan Cross",
-    googleFormUrl: ""
-};
